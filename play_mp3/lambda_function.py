b67dcefa12865f4e2930ed659dfe581dc27f9793
Sudhanshu Garg <esgarg@gmail.com>
2017-12-31 00:37:44 +0530

    minor fix

--- play_mp3/lambda_function.py
+++ play_mp3/lambda_function.py
@@ -117,8 +117,7 @@ def get_welcome_response():
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for listening to [INSERT:album_name]. " \
-                    "Hope you are more relaxed! "
+    speech_output = "Thank you for listening to [INSERT:whatever]." 
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
